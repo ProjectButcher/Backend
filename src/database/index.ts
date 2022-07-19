5252e7bf0015b0d1ba0467e5af87fd0c1c1ba761
Gustavo Riedo <gh03012@gmail.com>
2022-07-19 19:21:28 -0300

    refactor: removing some comments

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,6 +1,3 @@
-// This script is the one who creates a connection between TypeORM and Postgres
-// You should use "ormconfig.json" to configure your connection
-
-import { createConnection } from 'typeorm' // TypeORM is used to facilitate all database actions
+import { createConnection } from 'typeorm'
 
 createConnection()
