4bf0e6bd2ad38147f19b2a64ad8a281fb5b1b4e7
Gustavo Riedo <gh03012@gmail.com>
2022-07-20 16:32:54 -0300

    refactor: minor code style formating

--- src/database/migrations/1658002616868-CreateUsersTable.ts
+++ src/database/migrations/1658002616868-CreateUsersTable.ts
@@ -1,88 +1,88 @@
-import { MigrationInterface, QueryRunner, Table } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
 
 export class CreateUsersTable1658002616868 implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<void> {
+  public async up (queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: "users",
+        name: 'users',
         columns: [
           {
-            name: "id",
-            type: "uuid",
-            isPrimary: true,
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true
           },
           {
-            name: "first_name",
-            type: "varchar",
+            name: 'first_name',
+            type: 'varchar'
           },
           {
-            name: "last_name",
-            type: "varchar",
+            name: 'last_name',
+            type: 'varchar'
           },
           {
-            name: "org_name",
-            type: "varchar",
+            name: 'org_name',
+            type: 'varchar'
           },
           {
-            name: "user_type",
-            type: "enum",
-            enum: ["admin", "user"],
+            name: 'user_type',
+            type: 'enum',
+            enum: ['admin', 'user']
           },
           {
-            name: "address",
-            type: "varchar",
-            isNullable: true,
+            name: 'address',
+            type: 'varchar',
+            isNullable: true
           },
           {
-            name: "city",
-            type: "varchar",
-            isNullable: true,
+            name: 'city',
+            type: 'varchar',
+            isNullable: true
           },
           {
-            name: "state",
-            type: "varchar",
-            isNullable: true,
+            name: 'state',
+            type: 'varchar',
+            isNullable: true
           },
           {
-            name: "zip",
-            type: "varchar",
-            isNullable: true,
+            name: 'zip',
+            type: 'varchar',
+            isNullable: true
           },
           {
-            name: "country",
-            type: "varchar",
-            isNullable: true,
+            name: 'country',
+            type: 'varchar',
+            isNullable: true
           },
           {
-            name: "email",
-            type: "varchar",
-            isUnique: true,
+            name: 'email',
+            type: 'varchar',
+            isUnique: true
           },
           {
-            name: "password",
-            type: "varchar",
+            name: 'password',
+            type: 'varchar'
           },
           {
-            name: "last_password",
-            type: "varchar",
-            default: "''",
+            name: 'last_password',
+            type: 'varchar',
+            default: "''"
           },
           {
-            name: "created_at",
-            type: "timestamp",
-            default: "now()",
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()'
           },
           {
-            name: "updated_at",
-            type: "timestamp",
-            default: "now()",
-          },
-        ],
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()'
+          }
+        ]
       })
-    );
+    )
   }
 
-  public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable("users");
+  public async down (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users')
   }
 }
